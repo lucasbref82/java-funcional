279c1593bffb5ba2fbda90b8b82b2e14a275666a
Lucas Brenner <lucasbref82@gmail.com>
2025-06-29 07:10:09 -0300

    Fazendo alguns testes com stream

---
+++ src/straeam_v2/Main.java
@@ -0,0 +1,26 @@
+package straeam_v2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(3, 4, 5, 8, 10);
+        Stream<Integer> stream1 = list.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(stream1.toArray()));
+
+        int soma = list.stream().reduce(0, Integer::sum);
+        System.out.println(soma);
+
+        List<Integer> novaLista = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10)
+                .toList();
+
+        System.out.println(Arrays.toString(novaLista.toArray()));
+
+
+    }
+}
