df28be55f713662c894345a57085b46a09fb9f6e
Lucas Brenner <lucasbref82@gmail.com>
2025-07-02 06:28:59 -0300

    Usando o combine no reduce

--- src/estudo_2/reduce/Main.java
+++ src/estudo_2/reduce/Main.java
@@ -3,6 +3,7 @@ package estudo_2.reduce;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.DoubleStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,19 +16,41 @@ public class Main {
 
         // Reduce soma
         Optional<Integer> soma = list.parallelStream().reduce(Integer::sum);
-        soma.ifPresent(System.out::println);
 
         // Reduce multiplicador
         Optional<Integer> multiplicador = list.parallelStream().reduce((n1, n2) -> n1 * n2);
-        multiplicador.ifPresent(System.out::println);
 
         // Reduce concatenação
         Optional<String> concatenacao = listString.parallelStream().reduce(String::concat);
-        concatenacao.ifPresent(System.out::println);
 
         // Não associativas (Não podemos transformar em paralelo)
         Optional<Integer> subtracao = list.parallelStream().reduce((n1, n2) -> n1 - n2);
-        subtracao.ifPresent(System.out::println);
+
+
+        // Passando o valor de identidade
+        Integer soma2 = list.stream().reduce(0, Integer::sum);
+
+        // Passando o valor de identidade
+        Integer multiplicador2 = list.stream().reduce(1, (n1, n2) -> n1 * n2);
+
+        // Reduce concatenação
+        String concatenacao2 = listString.stream().reduce("", String::concat);
+
+        // DoubleStream
+        double menorValor = DoubleStream.of(1.5, 1,7, 6,7)
+                .reduce(Double.POSITIVE_INFINITY, Math::min);
+
+        // Usando combine
+
+        // Primeiro caso, sem combine.
+        String combineUm = list.stream().map(String::valueOf).reduce("", String::concat);
+        System.out.println(combineUm);
+
+        // Segundo caso, com combine
+        String combine2 = list.stream().reduce("", (x, y) -> x.concat(y.toString()), String::concat);
+        System.out.println(combine2);
+
+
 
 
 
